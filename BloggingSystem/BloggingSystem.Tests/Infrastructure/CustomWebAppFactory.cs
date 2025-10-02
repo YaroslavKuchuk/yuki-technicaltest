14ff9b3f4781a885a6bd5107995ef852b9dab207
YaroslavKuchuk <yaroslav.kuchuk@gmail.com>
2025-10-02 15:08:50 +0200

    6-testing: integration tests

---
+++ BloggingSystem/BloggingSystem.Tests/Infrastructure/CustomWebAppFactory.cs
@@ -0,0 +1,24 @@
+﻿// TODO: adjust namespaces/types
+using Blog.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace BloggingSystem.Tests.Infrastructure;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType.FullName?.Contains("DbContextOptions") == true);
+            if (descriptor != null) services.Remove(descriptor);
+
+            // TODO: replace BlogDbContext with your context type
+            services.AddDbContext<BlogDbContext>(o => o.UseInMemoryDatabase("api-tests"));
+        });
+    }
+}
---
+++ BloggingSystem/BloggingSystem.Tests/Infrastructure/ModelValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingSystem.Tests.Infrastructure;
+
+public static class ModelValidation
+{
+    public static IList<ValidationResult> Validate(object model)
+    {
+        var ctx = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+        Validator.TryValidateObject(model, ctx, results, validateAllProperties: true);
+        return results;
+    }
+}
